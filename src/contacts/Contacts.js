ed555bf2f71228683e074b097ce336dd11d4c2d2
RainHard23 <moiredmi7@gmail.com>
2023-08-16 00:17:04 +0300

    add vercel.json

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,21 +1,52 @@
 import React from 'react';
+import {useForm} from 'react-hook-form'; // Импортируйте хук useForm
 import style from './Contacts.module.scss'
 import styleContainer from './../common/styles/Container.module.css'
 import Title from "../common/components/title/Title";
 import Fade from "react-reveal/Fade";
+import axios from "axios";
+
 const Contacts = () => {
+    const {
+        register,
+        handleSubmit,
+    } = useForm(); // Инициализация хука
+
+    const onSubmit = (data) => {
+        console.log(data.firstName)
+        axios.post("http://localhost:3010/sendMessage", {
+            firstName: data.firstName,
+            email: data.email,
+            message: data.message
+        })
+            .then(()=> {
+                alert('pudge')
+            })
+    };
+
     return (
         <div id='contacts' className={style.contactsBlock}>
             <Fade bottom>
-            <div className={`${styleContainer.container} ${style.contactsContainer}`}>
-                <Title title={"Contacts"}/>
-                <form action="" className={style.formBlock}>
-                    <input placeholder={'Name'} type="text"/>
-                    <input placeholder={'E-mail'} type="text"/>
-                    <textarea placeholder={'Your message'} name="" rows="7" ></textarea>
-                    <button type='submit'>Send message</button>
-                </form>
-            </div>
+                <div className={`${styleContainer.container} ${style.contactsContainer}`}>
+                    <Title title={"Contacts"}/>
+                    <form onSubmit={handleSubmit(onSubmit)} className={style.formBlock}>
+                        <input
+                            {...register ("firstName", {
+                            })}
+                        />
+                        <input
+                            {...register ("email", {
+                            })}
+                        />
+                        <textarea
+                            rows={'7'}
+                            {...register ("message", {
+
+                            })}
+                        ></textarea>
+                        <button type='submit'>Send message</button>
+                    </form>
+                </div>
             </Fade>
         </div>
     );
